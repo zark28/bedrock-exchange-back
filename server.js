6d234e8deccbb76d743d8d3734c8018296aae369
zark28 <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>
2022-06-01 12:15:21 +0000

    fix: refactor hard coded connection string to Heroku config var 3

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ const authRoutes = require('./routers/authRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/', (req, res) => {
-  res.json('hello to blog');
-});
 app.use('/auth', authRoutes);
 app.use('/blogs', blogRoutes);
 
